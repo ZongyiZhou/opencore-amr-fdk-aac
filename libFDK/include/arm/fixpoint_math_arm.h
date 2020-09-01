d49e04a64f65b6fca22b24207f47df1d0a8919f7
Zongyi Zhou <zongyiz@nvidia.com>
2020-08-31 19:21:05 -0700

    Optimized fixpoint math functions for ARM
    1) Improve lrint, invSqrtNorm2, invFixp
    2) Add schur_div, fMultNorm, fDivNorm

--- libFDK/include/arm/fixpoint_math_arm.h
+++ libFDK/include/arm/fixpoint_math_arm.h
@@ -114,25 +114,26 @@ inline int lrint_fast(double v) {
   );
   return r;
 #else
-  int64x1_t x = vcvtn_s64_f64((float64x1_t){v});
-  return vqmovnd_s64(x[0]);
+  int64_t x = vcvtnd_s64_f64(v);
+  return vqmovnd_s64(x);
 #endif // __GNUC__
 }
 
 #define FUNCTION_sqrtFixp
 inline FIXP_DBL sqrtFixp(const FIXP_DBL op) {
-  return lrint(sqrt((INT64)op << 31));
+  return lrint(sqrt((double)((INT64)op << 31)));
 }
 
+#if defined(__llvm__) || __GNUC__ >= 7
 #define FUNCTION_invSqrtNorm2
 inline FIXP_DBL invSqrtNorm2(FIXP_DBL op_m, INT *result_e) {
   union {
     double d;
     UINT64 i;
   } result;
-  if (op_m == (FIXP_DBL)0) {
+  if (op_m == 0) {
     *result_e = 16;
-    return ((LONG)0x7fffffff);
+    return 0x7fffffff;
   }
   // d = op_m / 2.0;
 #ifdef __GNUC__
@@ -165,14 +166,21 @@ inline FIXP_DBL invFixp(FIXP_DBL op) {
 }
 
 inline FIXP_DBL invFixp(FIXP_DBL op_m, int *op_e) {
+  if (op_m == 0) {
+    *op_e = 31;
+    return ((LONG)0x7fffffff);
+  }
+#ifdef __GNUC__
+  INT cls;
+  __asm__("cls %w0, %w1" : "=r"(cls) : "r"(op_m - 1));
+  INT64 r = ((1LL << 61) >> cls) / op_m;
+  *op_e = cls + 1 - *op_e;
+  return (FIXP_DBL)r;
+#else
   union {
     double d;
     UINT64 i;
   } result;
-  if ((op_m == (FIXP_DBL)0) || (op_m == (FIXP_DBL)1)) {
-    *op_e = 31 - *op_e;
-    return ((LONG)0x7fffffff);
-  }
   // Use 2 Newton-Ralphson iterations to reach 36-bit precision
   float64x1_t d = {(double)op_m};
   float64x1_t x = vrecpe_f64(d);
@@ -182,9 +190,84 @@ inline FIXP_DBL invFixp(FIXP_DBL op_m, int *op_e) {
   INT mant = ((INT)(result.i >> 22) & 0x3FFFFFFF) | 0x40000000;
   *op_e = ((INT)(result.i >> 52) & 0x7FF) - 1023 + 32 - *op_e;
   return (INT64)result.i < 0 ? -mant : mant;
+#endif
+}
+#endif // defined(__llvm__) || __GNUC__ >= 7
+
+#define FUNCTION_schur_div
+inline FIXP_DBL schur_div(FIXP_DBL num, FIXP_DBL denum, INT) {
+  if ((UINT)num >= (UINT)denum) return MAXVAL_DBL;
+
+  return (FIXP_DBL)(((UINT64)num << 31) / (UINT)denum);
 }
 
-#elif defined(__ARM_ARCH_5TE__)
+#define FUNCTION_fMultNorm
+inline FIXP_DBL fMultNorm(FIXP_DBL f1, FIXP_DBL f2, INT *result_e) {
+  INT64 product = (INT64)f1 * f2;
+  if (product == 0) return 0;
+
+  INT cls;
+#ifdef __GNUC__
+  __asm__("cls %x0, %x1" : "=r"(cls) : "r"(product));
+#else
+  cls = fNormz(product ^ (product >> 63));
+#endif
+  *result_e = 1 - cls;
+  return (FIXP_DBL)(product << cls >> 32);
+}
+
+inline FIXP_DBL fMultNorm(FIXP_DBL f1, FIXP_DBL f2) { return fMult(f1, f2); }
+
+inline FIXP_DBL fMultNorm(FIXP_DBL f1_m, INT f1_e, FIXP_DBL f2_m, INT f2_e,
+                          INT result_e) {
+  INT64 product = (INT64)f1_m * f2_m;
+  if (product == 0) return 0;
+
+  INT64 scalefactor = f1_e + f2_e - result_e - 31;
+  product = vqrshld_s64(product, scalefactor);
+  return vqmovnd_s64(product);
+}
+
+#define FUNCTION_fMultI
+inline INT fMultI(FIXP_DBL a, INT b) {
+  INT64 r = (INT64)a * b + (1 << 30);
+  return (INT)(r >> 31);
+}
+
+#ifdef __GNUC__
+#define FUNCTION_fLog2
+FDK_INLINE FIXP_DBL fLog2(FIXP_DBL x_m, INT x_e, INT *result_e) {
+  if (x_m <= FL2FXCONST_DBL(0.0f)) {
+    *result_e = DFRACT_BITS - 1;
+    x_m = FL2FXCONST_DBL(-1.0f);
+  } else {
+    UINT clz = fixnormz_D(x_m);
+    int offset = x_e - clz;
+    UINT mant = x_m << clz << 1;
+    UINT index = mant >> (DFRACT_BITS - LOG2_LUT_SIZE_LOG2);
+    UINT frac = mant & LOG2_LUT_MASK;
+    UINT64 r = log2_lut[index] * (UINT64)(LOG2_LUT_MASK + 1 - frac) +
+               log2_lut[index + 1] * (UINT64)frac;
+    int norm;
+    __asm__("cls %w0, %w1" : "=r"(norm) : "r"(offset + 1));
+    x_m = offset << (norm - 1);
+    *result_e = DFRACT_BITS - norm;
+    const UINT round_diff = (1 << (LOG2_RESULT_SCALE_BITS - 1)) -
+                            (1 << (63 - LOG2_LUT_SIZE_LOG2 - norm));
+    x_m += (r - round_diff) >> (64 - LOG2_LUT_SIZE_LOG2 - norm);
+  }
+  return x_m;
+}
+
+FDK_INLINE FIXP_DBL fLog2_lookup(FIXP_DBL x_m, INT x_e);
+
+FDK_INLINE FIXP_DBL fLog2(FIXP_DBL x_m, INT x_e) {
+  return fLog2_lookup(x_m, x_e);
+}
+#endif
+
+#else // 32-bit ARM
+#ifdef __ARM_ARCH_5TE__
 
 #ifdef __GNUC__
 #define lrint(v) lrint_fast(v)
@@ -196,36 +279,18 @@ inline int lrint_fast(double v) {
   );
   return x;
 }
-
-#define FUNCTION_llRightShift32
-inline UINT llRightShift32(UINT64 value, INT shift) {
-  union {
-    UINT64 u64;
-    struct {
-      UINT l32;
-      UINT h32;
-    };
-  } v = {value};
-  INT s;
-  __asm__(
-    "rsb %1, %1, #32\n\t"
-    "lsr %0, %0, %3\n\t"
-#if __thumb__
-    "lsl %1, %2, %1\n\t"
-    "orr %0, %0, %1"
-#else
-    "orr %0, %0, %2, lsl %1"
 #endif
-    : "+r"(v.l32), "=&r"(s)
-    : "r"(v.h32), "r"(shift)
-  );
-  return v.l32;
+
+#define FUNCTION_schur_div
+inline FIXP_DBL schur_div(FIXP_DBL num, FIXP_DBL denum, INT) {
+  if ((UINT)num >= (UINT)denum) return MAXVAL_DBL;
+
+  return (INT)((double)num * (1U << 31) / (double)denum);
 }
-#endif // __GNUC__
 
 #define FUNCTION_sqrtFixp
 inline FIXP_DBL sqrtFixp(const FIXP_DBL op) {
-  FIXP_DBL result = lrint(sqrt(op) * sqrt(1UL<<31));
+  FIXP_DBL result = lrint(sqrt(op) * sqrt(1UL << 31));
   return result;
 }
 
@@ -235,9 +300,9 @@ inline FIXP_DBL invSqrtNorm2(FIXP_DBL op_m, INT *result_e) {
     double d;
     UINT64 i;
   } result;
-  if (op_m == (FIXP_DBL)0) {
+  if (op_m == 0) {
     *result_e = 16;
-    return ((LONG)0x7fffffff);
+    return 0x7fffffff;
   }
   result.d = sqrt(2) / sqrt(op_m);
   UINT mant = ((UINT)(result.i >> 22) & 0x3FFFFFFF) | 0x40000000;
@@ -247,7 +312,7 @@ inline FIXP_DBL invSqrtNorm2(FIXP_DBL op_m, INT *result_e) {
 
 #define FUNCTION_invFixp
 inline FIXP_DBL invFixp(FIXP_DBL op) {
-  if ((op == (FIXP_DBL)0) || (op == (FIXP_DBL)1)) {
+  if ((UINT)op <= 1) {
     return 0x7fffffff;
   }
 #ifdef __ARM_FEATURE_IDIV
@@ -263,7 +328,7 @@ inline FIXP_DBL invFixp(FIXP_DBL op_m, int *op_e) {
     UINT64 i;
     INT i32[2];
   } result;
-  if ((op_m == (FIXP_DBL)0) || (op_m == (FIXP_DBL)1)) {
+  if ((UINT)op_m <= 1) {
     *op_e = 31 - *op_e;
     return ((LONG)0x7fffffff);
   }
@@ -273,6 +338,171 @@ inline FIXP_DBL invFixp(FIXP_DBL op_m, int *op_e) {
   return (INT)result.i32[1] < 0 ? -mant : mant;
 }
 
+#endif  // __ARM_ARCH_5TE__
+
+#define FUNCTION_fMultNorm
+inline FIXP_DBL fMultNorm(FIXP_DBL f1, FIXP_DBL f2, INT *result_e) {
+  if ((f1 | f2) == 0) return 0;
+  union {
+    INT64 i64;
+    struct {
+      UINT l32;
+      INT h32;
+    };
+  } product;
+  product.i64 = (INT64)f1 * f2;
+
+  INT sign = product.h32 >> 31;
+  INT t = product.h32 ^ sign;
+  INT cls = fixnormz_D(t) - 1;
+  if (cls == 31) cls += fixnormz_D(product.l32 ^ sign);
+  *result_e = 1 - cls;
+  if (cls < 32) {
+#ifdef __GNUC__
+    __asm__(
+      "rsb %1, %3, #32\n\t"
+      "lsl %0, %0, %3\n\t"
+#if __thumb__
+      "lsr %1, %2, %1\n\t"
+      "orr %0, %0, %1"
+#else
+      "orr %0, %0, %2, lsr %1"
+#endif
+      : "+r"(product.h32), "=&r"(t)
+      : "r"(product.l32), "r"(cls)
+    );
+#else
+    product.i64 <<= cls;
+#endif
+    return product.h32;
+  } else {
+    return product.l32 << (cls - 32);
+  }
+}
+
+inline FIXP_DBL fMultNorm(FIXP_DBL f1, FIXP_DBL f2) { return fMult(f1, f2); }
+
+inline FIXP_DBL fMultNorm(FIXP_DBL f1_m, INT f1_e, FIXP_DBL f2_m, INT f2_e,
+                          INT result_e) {
+  if ((f1_m | f2_m) == 0) return 0;
+
+  union {
+    INT i32[2];
+    INT64 i64;
+    double d;
+  } scalefactor;
+#ifdef __ARM_ARCH_7_A__
+  INT64 product = (INT64)f1_m * f2_m;
+  scalefactor.i32[0] = f1_e + f2_e - result_e - 31;
+  scalefactor.i32[1] = scalefactor.i32[0] >> 31;
+  int64x1_t t = vqrshl_s64((int64x1_t){product}, (int64x1_t){scalefactor.i64});
+  int32x2_t r = vqmovn_s64(vcombine_s64(t, (int64x1_t){0}));
+  return r[0];
+#else
+  scalefactor.i32[0] = 0;
+  scalefactor.i32[1] = (f1_e + f2_e - result_e - 31 + 1023) << 20;
+  double d = (double)f1_m * f2_m * scalefactor.d;
+  return lrint(d);
+#endif
+}
+
+#ifdef __GNUC__
+#define FUNCTION_llRightShift32
+inline UINT llRightShift32(UINT64 value, INT shift) {
+  union {
+    UINT64 u64;
+    struct {
+      UINT l32;
+      UINT h32;
+    };
+  } v = {value};
+  INT s;
+  __asm__(
+    "rsb %1, %3, #32\n\t"
+    "lsr %0, %0, %3\n\t"
+#if __thumb__
+    "lsl %1, %2, %1\n\t"
+    "orr %0, %0, %1"
+#else
+    "orr %0, %0, %2, lsl %1"
+#endif
+    : "+r"(v.l32), "=&r"(s)
+    : "r"(v.h32), "r"(shift)
+  );
+  return v.l32;
+}
+#endif // __GNUC__
+
 #endif // __ARM_ARCH_8__
+
+#define FUNCTION_fMultIfloor
+inline INT fMultIfloor(FIXP_DBL a, INT b) { return fMult(a, (FIXP_DBL)b); }
+
+#if defined(__ARM_ARCH_8__) || defined(__ARM_ARCH_5TE__)
+#define FUNCTION_fDivNorm
+inline FIXP_DBL fDivNorm(FIXP_DBL L_num, FIXP_DBL L_denum, INT *result_e) {
+  FDK_ASSERT(L_num >= (FIXP_DBL)0);
+  FDK_ASSERT(L_denum > (FIXP_DBL)0);
+
+  if (L_num == 0) {
+    *result_e = 0;
+    return 0;
+  }
+
+  union {
+    double d;
+    UINT64 i;
+    INT i32[2];
+  } result;
+  result.d = (double)L_num / L_denum;
+
+  INT mant = ((INT)(result.i >> 22) & 0x3FFFFFFF) | 0x40000000;
+#ifdef __ARM_ARCH_8__
+  *result_e = ((INT)(result.i >> 52) & 0x7FF) - 1023 + 1;
+#else
+  *result_e = ((result.i32[1] >> 20) & 0x7FF) - 1023 + 1;
+#endif  // __ARM_ARCH_8__
+  return mant;
+}
+
+inline FIXP_DBL fDivNorm(FIXP_DBL num, FIXP_DBL denom) {
+  return schur_div(num, denom, 31);
+}
+
+#define FUNCTION_fDivNormSigned
+inline FIXP_DBL fDivNormSigned(FIXP_DBL L_num, FIXP_DBL L_denum,
+                               INT *result_e) {
+  FDK_ASSERT(L_denum);
+  if (L_num == 0) {
+    *result_e = 0;
+    return 0;
+  }
+
+  union {
+    double d;
+    UINT64 i;
+    INT i32[2];
+  } result;
+  result.d = (double)L_num / L_denum;
+
+  INT mant = ((INT)(result.i >> 22) & 0x3FFFFFFF) | 0x40000000;
+#ifdef __ARM_ARCH_8__
+  INT sign = (INT64)result.i >> 63;
+  *result_e = ((INT)(result.i >> 52) & 0x7FF) - 1023 + 1;
+#else
+  INT sign = result.i32[1] >> 31;
+  *result_e = ((result.i32[1] >> 20) & 0x7FF) - 1023 + 1;
+#endif  // __ARM_ARCH_8__
+  return (FIXP_DBL)((mant ^ sign) - sign);
+}
+
+inline FIXP_DBL fDivNormSigned(FIXP_DBL num, FIXP_DBL denom) {
+  INT sign_n = num >> 31, sign_d = denom >> 31;
+  INT r = schur_div(num ^ sign_n - sign_n, denom ^ sign_d - sign_d, 31);
+  sign_n ^= sign_d;
+  return r ^ sign_n - sign_n;
+}
+
+#endif  // defined(__ARM_ARCH_8__) || defined(__ARM_ARCH_5TE__)
 
 #endif /* !defined(FIXPOINT_MATH_ARM_H) */
