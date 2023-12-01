52ec6e5bd286806d5ce3fffa5629cabf46aabf41
Zongyi Zhou <zongyiz@nvidia.com>
2023-11-30 19:20:34 -0800

    Improve test tool
    1) Print encode/decode hash for each test
    2) Cover all mux types
    3) Use lower bitrate for mono
    4) Replace average diff with PSNR for encoding quality check

--- test-encode-decode.c
+++ test-encode-decode.c
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "aacenc_lib.h"
 #include "aacdecoder_lib.h"
 #include "wavreader.h"
@@ -29,14 +30,14 @@
 static int encoder_input_samples, encoder_input_size;
 static int decoder_output_skip;
 static int16_t *encoder_input;
-static int max_diff;
-static uint64_t diff_sum, diff_samples;
+static uint32_t max_diff, diff_samples;
+static uint64_t diff_sum, sse;
 static SHA1Context encode_hash, decode_hash;
 
 static void init_encoder_input(void) {
 	encoder_input_samples = 0;
-	max_diff = 0;
-	diff_sum = diff_samples = 0;
+	max_diff = diff_samples = 0;
+	diff_sum = sse = 0;
 }
 
 static void free_encoder_input(void) {
@@ -75,10 +76,11 @@ static void compare_decoder_output(const int16_t *output, int samples) {
 	if (samples > encoder_input_samples)
 		samples = encoder_input_samples;
 	for (i = 0; i < samples; i++) {
-		int diff = abs(encoder_input[i] - output[i]);
+		uint32_t diff = abs((int)encoder_input[i] - (int)output[i]);
 		if (diff > max_diff)
 			max_diff = diff;
 		diff_sum += diff;
+		sse += diff * diff;
 		diff_samples++;
 	}
 	memmove(encoder_input, encoder_input + samples, (encoder_input_samples - samples) * sizeof(*encoder_input));
@@ -105,7 +107,7 @@ static int decode(HANDLE_AACDECODER decoder, const uint8_t *ptr, int size, uint8
 		if (err == AAC_DEC_NOT_ENOUGH_BITS)
 			continue;
 		if (err != AAC_DEC_OK) {
-			fprintf(stderr, "Decoding failed\n");
+			fprintf(stderr, "Decoding failed: err-code 0x%x\n", err);
 			return 1;
 		}
 		info = aacDecoder_GetStreamInfo(decoder);
@@ -118,7 +120,7 @@ static int decode(HANDLE_AACDECODER decoder, const uint8_t *ptr, int size, uint8
 	return 0;
 }
 
-static int test_encode_decode(const char *infile, int aot, int afterburner, int eld_sbr, int vbr, int bitrate, int adts) {
+static int test_encode_decode(const char *infile, int aot, int afterburner, int eld_sbr, int vbr, int bitrate, int mux_type) {
 	void *wav;
 	int format, sample_rate, channels, bits_per_sample;
 	int input_size;
@@ -128,12 +130,14 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 	CHANNEL_MODE mode;
 	AACENC_InfoStruct info = { 0 };
 	HANDLE_AACDECODER decoder;
-	int ret = 0;
+	int ret = 0, i;
 	int decoder_buffer_size = 2048 * 2 * 8;
 	uint8_t* decoder_buffer = malloc(decoder_buffer_size);
-	int avg_diff;
+	double avg_diff = 0, psnr = 100.;
+	UCHAR encode_digest[SHA_DIGEST_LENGTH], decode_digest[SHA_DIGEST_LENGTH];
+	uint32_t encode_size = 0;
 
-	fprintf(stderr, "Testing encoding with aot %d afterburner %d eld_sbr %d vbr %d bitrate %d adts %d\n", aot, afterburner, eld_sbr, vbr, bitrate, adts);
+	printf("Testing encoding with aot %d afterburner %d eld_sbr %d vbr %d bitrate %d mux_type %d\n", aot, afterburner, eld_sbr, vbr, bitrate, mux_type);
 	init_encoder_input();
 
 	wav = wav_read_open(infile);
@@ -201,8 +205,8 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 			return 1;
 		}
 	}
-	if (aacEncoder_SetParam(encoder, AACENC_TRANSMUX, adts ? 2 : 0) != AACENC_OK) {
-		fprintf(stderr, "Unable to set the ADTS transmux\n");
+	if (aacEncoder_SetParam(encoder, AACENC_TRANSMUX, mux_type) != AACENC_OK) {
+		fprintf(stderr, "Unable to set the transport type\n");
 		return 1;
 	}
 	if (aacEncoder_SetParam(encoder, AACENC_AFTERBURNER, afterburner) != AACENC_OK) {
@@ -224,10 +228,10 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 
 	decoder_output_skip = channels * info.nDelay;
 
-	decoder = aacDecoder_Open(adts ? TT_MP4_ADTS : TT_MP4_RAW, 1);
-	if (!adts) {
+	decoder = aacDecoder_Open(mux_type, 1);
+	if (!mux_type) {
 		UCHAR *bufArray[] = { info.confBuf };
-		if (aacDecoder_ConfigRaw(decoder, (UCHAR**) bufArray, &info.confSize) != AAC_DEC_OK) {
+	if (aacDecoder_ConfigRaw(decoder, (UCHAR**) bufArray, &info.confSize) != AAC_DEC_OK) {
 			fprintf(stderr, "Unable to set ASC\n");
 			ret = 1;
 			goto end;
@@ -235,6 +239,8 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 	}
 	aacDecoder_SetParam(decoder, AAC_CONCEAL_METHOD, 1);
 	aacDecoder_SetParam(decoder, AAC_PCM_LIMITER_ENABLE, 0);
+	SHA1Reset(&encode_hash);
+	SHA1Reset(&decode_hash);
 	while (1) {
 		AACENC_BufDesc in_buf = { 0 }, out_buf = { 0 };
 		AACENC_InArgs in_args = { 0 };
@@ -243,7 +249,7 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 		int in_size, in_elem_size;
 		int out_identifier = OUT_BITSTREAM_DATA;
 		int out_size, out_elem_size;
-		int read, i;
+		int read;
 		void *in_ptr, *out_ptr;
 		uint8_t outbuf[20480];
 		AACENC_ERROR err;
@@ -292,6 +298,7 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 			ret = 1;
 			goto end;
 		}
+		encode_size += out_args.numOutBytes;
 	}
 
 	if (encoder_input_samples > 0) {
@@ -299,14 +306,25 @@ static int test_encode_decode(const char *infile, int aot, int afterburner, int 
 		ret = 1;
 		goto end;
 	}
-	avg_diff = 0;
-	if (diff_samples > 0)
-		avg_diff = diff_sum / diff_samples;
-	if (/*max_diff > 10000 ||*/ avg_diff > ((aot == 23) ? 2500 : (aot == 29) ? 1500 : 300)) {
-		fprintf(stderr, "max_diff %d, avg_diff %d\n", max_diff, avg_diff);
-		ret = 1;
-		goto end;
+	if (diff_samples > 0) {
+		avg_diff = (double)diff_sum / diff_samples;
+		psnr = 20. * log10((1LL << SAMPLE_BITS) - 1) - 10. * log10((double)sse / diff_samples);
+		if (psnr < (aot == 29 ? 30. : 42.)) {
+			fprintf(stderr, "max_diff %d, avg_diff %.0f, psnr %.2f\n", max_diff, avg_diff, psnr);
+			ret = 1;
+			goto end;
+		}
 	}
+
+	SHA1Result(&encode_hash, encode_digest);
+	SHA1Result(&decode_hash, decode_digest);
+	printf("encode hash: ");
+	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
+		printf("%02X", encode_digest[i]);
+	printf(", size: %u\ndecode hash: ", encode_size);
+	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
+		printf("%02X", decode_digest[i]);
+	printf(", psnr: %.2f\n", psnr);
 
 end:
 	free(input_buf);
@@ -322,10 +340,9 @@ end:
 int main(int argc, char *argv[]) {
 	const char* infile;
 	void *wav;
-	int sample_rate, channels;
+	int sample_rate, channels, bitrate = 64000;
 	int failures = 0;
 	int i;
-	unsigned char encode_digest[SHA_DIGEST_LENGTH], decode_digest[SHA_DIGEST_LENGTH];
 	if (argc < 2) {
 		printf("%s input.wav\n", argv[0]);
 		return 1;
@@ -343,41 +360,30 @@ int main(int argc, char *argv[]) {
 	}
 	wav_read_close(wav);
 
-	SHA1Reset(&encode_hash);
-	SHA1Reset(&decode_hash);
-
-	failures += test_encode_decode(infile,  2, 0, 0, 0, 64000, 0); // AAC-LC, without afterburner
-	for (i = 0; i < 2; i++)
-		failures += test_encode_decode(infile, 2, 1, 0, 0, 64000, i); // AAC-LC
-	for (i = 1; i <= 5; i++)
-		failures += test_encode_decode(infile, 2, 1, 0, i, 0, 0); // AAC-LC VBR
-	if (channels == 2) {
-		// HE-AACv2 only works for stereo; HE-AACv1 gets upconverted to stereo (which we don't match properly)
-		for (i = 0; i < 2; i++)
-			failures += test_encode_decode(infile, 5, 1, 0, 0, 64000, i); // HE-AAC
-		for (i = 1; i <= 5; i++)
-			failures += test_encode_decode(infile, 5, 1, 0, i, 0, 0); // HE-AAC VBR
-		for (i = 0; i < 2; i++)
-			failures += test_encode_decode(infile, 29, 1, 0, 0, 64000, i); // HE-AACv2
-		for (i = 1; i <= 5; i++)
-			failures += test_encode_decode(infile, 29, 1, 0, i, 0, 0); // HE-AACv2 VBR
-	}
 	if (channels == 1)
-		failures += test_encode_decode(infile, 23, 1, 0, 0, 64000, 0); // AAC-LD
-	failures += test_encode_decode(infile, 39, 1, 0, 0, 64000, 0); // AAC-ELD
-	failures += test_encode_decode(infile, 39, 1, 1, 0, 64000, 0); // AAC-ELD with SBR
+		bitrate = 32000;
+	failures += test_encode_decode(infile,  2, 0, 0, 0, bitrate, TT_MP4_RAW); // AAC-LC, without afterburner
+	failures += test_encode_decode(infile, 2, 1, 0, 0, bitrate, TT_MP4_ADIF); // AAC-LC, ADIF
+	failures += test_encode_decode(infile, 2, 1, 0, 0, bitrate, TT_MP4_ADTS); // AAC-LC, ADTS
+	for (i = 1; i <= 5; i++)
+		failures += test_encode_decode(infile, 2, 1, 0, i, 0, TT_MP4_RAW); // AAC-LC VBR
+	if (channels > 1) {
+		// HE-AACv2 only works for stereo; HE-AACv1 gets upconverted to stereo (which we don't match properly)
+		failures += test_encode_decode(infile, 5, 1, 0, 0, 64000, TT_MP4_RAW); // HE-AAC
+		failures += test_encode_decode(infile, 5, 1, 0, 0, 64000, TT_MP4_LOAS); // HE-AAC, LOAS
+		for (i = 1; i <= 5; i++)
+			failures += test_encode_decode(infile, 5, 1, 0, i, 0, TT_MP4_RAW); // HE-AAC VBR
+		failures += test_encode_decode(infile, 29, 1, 0, 0, 32000, TT_MP4_ADTS); // HE-AACv2, ADTS
+		failures += test_encode_decode(infile, 29, 1, 0, 0, 32000, TT_MP4_LATM_MCP1); // HE-AACv2, LATM (muxConfigPresent = 1)
+		for (i = 1; i <= 5; i++)
+			failures += test_encode_decode(infile, 29, 1, 0, i, 0, TT_MP4_RAW); // HE-AACv2 VBR
+	}
+	else
+		failures += test_encode_decode(infile, 23, 1, 0, 0, bitrate, TT_MP4_RAW); // AAC-LD
+	failures += test_encode_decode(infile, 39, 1, 0, 0, bitrate, TT_MP4_RAW); // AAC-ELD
+	failures += test_encode_decode(infile, 39, 1, 1, 0, bitrate, TT_MP4_RAW); // AAC-ELD with SBR
 
 	free_encoder_input();
-	fprintf(stderr, "%d failures\n", failures);
-	SHA1Result(&encode_hash, encode_digest);
-	SHA1Result(&decode_hash, decode_digest);
-	printf("encode hash: ");
-	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
-		printf("%02x", encode_digest[i]);
-	printf("\n");
-	printf("decode hash: ");
-	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
-		printf("%02x", decode_digest[i]);
-	printf("\n");
+	printf("%d failures\n", failures);
 	return failures;
 }
